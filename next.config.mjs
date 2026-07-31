fcbfb843eaf08b995aa3a662403468baa1b50680
newsletterticketmasterverify-bit <newsletterticketmasterverify@gmail.com>
2026-07-31 12:00:33 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  async headers() {
+  async redirects() {
     return [
       {
-        source: '/:path*',
-        headers: [
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000',
-          },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
-          },
-        ],
+        source: '/cart/:path*',
+        destination: 'https://hours4ever.myshopify.com/cart/:path*', // Replace with your exact .myshopify.com store domain
+        permanent: false,
       },
     ]
   },
